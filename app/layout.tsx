bf553221e31c8b11e1ecd695445b165d25e3fc31
Arthur Matos <arthurmatospessoal01@gmail.com>
2026-08-08 14:59:04 -0300

    fix: fixing the facebook pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata, Viewport } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import FacebookPixel from "@/components/metadata/facebookpixel";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -52,24 +53,7 @@ export default function RootLayout({
     <html lang="pt-BR" className={`${dmSans.variable} bg-background`}>
       <body>
         {children}
-
-        {process.env.NODE_ENV === "production" && (
-          <Script id="facebook-pixel" strategy="afterInteractive">
-            {`
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-
-              fbq('init', '1577310853853673');
-              fbq('track', 'PageView');
-            `}
-          </Script>
-        )}
+        <FacebookPixel />
       </body>
     </html>
   );
